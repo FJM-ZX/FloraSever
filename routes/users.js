53327893d613cdba73c4d8a0ef300d450d1e0604
Fu Jiaming <478560836@163.com>
2018-10-18 18:48:26 +0800

    mongo db

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.header("Content-Type", "application/json;charset=utf-8");
+  MongoClient.connect(url,{useNewUrlParser:true},function(err, db) {
+    if (err) throw err;
+    var dbase = db.db("floradb");
+    var collection = dbase.collection('userAccount');
+    collection.find({}).toArray(function(err, result) { // 返回集合中所有数据
+        if (err) throw err;
+        res.send(result);
+        db.close();
+    });
+  });
 });
 
 module.exports = router;
